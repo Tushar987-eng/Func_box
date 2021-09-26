f5a60c4d0b644a13090a345fc8d367a1ac446202
Tushar <tusharsrivastav2001@gmail.com>
2021-09-26 23:37:06 +0530

    Added more files

--- app.js
+++ app.js
@@ -131,6 +131,11 @@ app.post("/submit",function(req,res){
         }
     });
 });
-app.listen(3000,function(){
-    console.log("Server Started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port,function(){
+    console.log("Server has started successfully");
 });
